aff9c818b97a79da77e06d73a13aa3e6869a38a8
John Beene <JohnBeeneNYC@gmail.com>
2014-06-02 20:27:53 -0400

    added yellow border to nav dots

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -1,6 +1,38 @@
-$(document).ready(function() {
+function setNav() {
+  var pageHeight = $('.page').outerHeight();
+  var dividerHeight = $('.divider').outerHeight();
+  var totalHeight = pageHeight + dividerHeight;
+
+  var page = Math.ceil(($(window).scrollTop() - (pageHeight-200))/totalHeight) + 1;
+  if (('#'+ page) != $('.active').attr('id')) {
+    $('.nav-li').removeClass('active');
+    $('#' + page).addClass('active');     
+  };
+};
+
+function navPointer() {
+  var visited = sessionStorage["visited"]
+  if(!visited) {
+    sessionStorage["visited"] = true
+    window.setTimeout(function() {
+      $('#nav-pointer').fadeIn(2000);
+      $('#nav-pointer').fadeOut(5000);
+    }, 5000);
+  };
+};
+
+function popup() {
   $('.nav-li').hover(function() {
     id = $(this).attr('id');
     $('#popup-' + id).toggle();
   });
+}
+
+$(document).ready(function() {
+  popup();
+  navPointer();
+  $(window).scrollTop();
+  $(window).scroll(setNav);
+  $('#home h1').fadeOut(7000);
+  $('#menu').fadeIn(7000);
 });
