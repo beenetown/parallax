5c828746b51333f80507570f043376b5fa69839a
John Beene <JohnBeeneNYC@gmail.com>
2014-06-25 11:39:39 -0400

    added compression and preloading to speed up initial site load

--- public/javascripts/imageSlider.js
+++ public/javascripts/imageSlider.js
@@ -1,20 +1,25 @@
 
+var firstTimeThrough = true;
 function timedSlider(images, time) {
   var i = 0;
-  setInterval(function(){
-    i ++;
-    if(i >= images.length) {
+  setInterval(function () {
+    i++;
+    if (i >= images.length) {
       i = 0;
-    };
-    $('#home').css({'background': "url('/public/images/large/" + images[i] + "') no-repeat center center fixed",  
+      firstTimeThrough = false;
+    }
+    if (firstTimeThrough) {
+      $('head').append('<img style="display: none;" src="/public/images/large/' + images[i + 1] + '">');
+    }
+    $('#home').css({'background': "url('/public/images/large/" + images[i] + "') no-repeat center center fixed",
                     "-webkit-background-size": "cover",
-                    "-moz-background-size": "cover", 
-                    "-o-background-size": "cover", 
+                    "-moz-background-size": "cover",
+                    "-o-background-size": "cover",
                     "background-size": "cover"});
   }, time);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
   if (!matchMedia('only screen and (max-device-width: 700px)').matches) {
     timedSlider(['acoustic_1920.png', 'desk_1920.png', 'drums_1920.png', 'mics_1920.png', 'speakers_1920.png', 'home_1920.png'], 7000);
   }
